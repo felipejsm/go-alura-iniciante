62e1c45a95725e63becbffa4fc734d1d6a16d7eb
felipejsm <felipejsm41@gmail.com>
2020-07-01 21:26:00 -0300

    Scanf :D

---
+++ menu.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Felipe"
+	idade := 30
+	versao := 1.1
+	fmt.Println("Olá Sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+
+	var comando int
+	fmt.Scanf("%d", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+}
