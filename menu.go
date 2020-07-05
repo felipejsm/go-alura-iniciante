9d3f1741aba9a9e1e9ecbfd79f480fc72de439f3
felipejsm <felipejsm41@gmail.com>
2020-07-05 13:46:55 -0300

    Usando for

--- menu.go
+++ menu.go
@@ -1,20 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br/askjdbahsbciahsbca"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
+}
 
 func main() {
+	exibeIntroducao()
+	for {
+		exibeMenu()
+		comando := leComando()
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+
+	}
+
+}
+func leComando() int {
+	var comando int
+	fmt.Scan(&comando)
+	return comando
+}
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func exibeIntroducao() {
 	nome := "Felipe"
 	idade := 30
 	versao := 1.1
 	fmt.Println("Olá Sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")
-
-	var comando int
-	//fmt.Scanf("%d", &comando)
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
 }
